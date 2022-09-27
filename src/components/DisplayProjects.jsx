d742d0207cd4ffd0c80f871718260a3719faf2f0
Theo Puteaux <theo.ptx@laposte.net>
2022-09-28 00:23:16 +0200

    display information of each project

--- src/components/DisplayProjects.jsx
+++ src/components/DisplayProjects.jsx
@@ -1,26 +1,55 @@
-import thumbnail from '../assets/testthumbnail.jpg'
+import { projectsData } from '../mock/data'
 
-export default function DisplayProject(){
-    return(
+export default function DisplayProject() {
+
+
+    const joinText = (tab) => {
+        const language = []
+        tab.forEach(info => {
+            language.push(info)
+        })
+        return language.join('/')
+    }
+    return (
         <div id="display-projects" className='container'>
             <div className="content">
                 <h2 className="title">Projets</h2>
-                <div className="display-projects">
-                    <div className="project">
-                        <h3 className="project-title">Title Projet</h3>
-                        <span className="project-description">
-                            <p>
-                                description projet
-                                ajout language utilisé
-                            </p>
-                            <button>Voir le code</button>
-                            <button>Acceder au site</button>                        
-                        </span>                        
-                    </div>
-                    <div className='thumbnail-project'>
-                        <img src={thumbnail} alt=""/>
-                    </div>
-                </div>
+
+                {projectsData.map((project) => {
+                    return (
+                        <div key={project.id+"project"} className="display-projects">
+                            <div className="project">
+                                <h3 className="project-title">{project.title}</h3>
+                                <span className="project-description">
+                                    <h4>{project.mission}</h4>
+                                    {
+                                        project.info.map((info, index) => {
+                                            return (
+                                                <p key={info+' '+index}>{info}</p>
+                                            )
+                                        })
+                                    }
+                                    <div>
+                                        {joinText(project.language)}
+                                    </div>
+                                    
+
+                                    <a href={project.repo}>
+                                        <button>Voir le code</button>
+                                    </a>
+                                    <a href={project.url}>
+                                        <button>Acceder au site</button>
+                                    </a>
+
+                                </span>
+                            </div>
+                            <div className='thumbnail-project'>
+                                <img src={require('../assets/project/'+project.img)} alt="" />
+                            </div>
+                        </div>
+                    )
+                })
+                }
             </div>
         </div>
     )
