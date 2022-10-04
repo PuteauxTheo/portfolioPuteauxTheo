5f3ebfd38460cd05548406ffce49ccbaa463f907
Theo Puteaux <theo.ptx@laposte.net>
2022-10-04 17:29:54 +0200

    add skills

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,12 +2,14 @@ import About from "../components/About";
 import DisplayProject from "../components/DisplayProjects";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Skills from "../components/Skills";
 
 export default function App(){
     return (
         <div id="app">
             <Header/>
             <About/>
+            <Skills/>
             <DisplayProject/>
             <Footer/>
         </div>
